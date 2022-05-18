0b9c660bb62ea512d965f5e4358b588f807bd6d2
Galya Somik <302490@gmail.com>
2022-05-18 20:15:41 +0600

    Add method for creating slides inside root slider element. Refactor.

--- src/Slider.ts
+++ src/Slider.ts
@@ -2,6 +2,7 @@ import { SliderOptions } from './models';
 
 export class Slider {
   options: SliderOptions;
+  rootElement: HTMLElement;
 
   constructor(options: SliderOptions) {
     this.options = options;
@@ -9,13 +10,24 @@ export class Slider {
 
   start(): void {
     this.setUpRootElement();
+    this.createSlides();
   }
 
   setUpRootElement(): void {
-    const rootElement: HTMLElement = document.querySelector(this.options.root) as HTMLElement;
-    rootElement.classList.add('slider')
-    rootElement.style.width = `${this.options.width}px`;
-    rootElement.style.height = `${this.options.height}px`;
+    this.rootElement = document.querySelector(this.options.root) as HTMLElement;
+    this.rootElement.classList.add('slider');
+    this.rootElement.style.width = `${this.options.width}px`;
+    this.rootElement.style.height = `${this.options.height}px`;
   }
+
+  createSlides(): void {
+    this.options.slides.forEach(slide => {
+      const slideElement: HTMLElement = document.createElement('div');
+      slideElement.classList.add('slider__slide');
+      slideElement.style.background = slide.color;
+      this.rootElement.append(slideElement);
+    })
+  }
+
 
 }
